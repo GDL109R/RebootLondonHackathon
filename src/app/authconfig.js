c275a60064e5d81769a159825db21781a9ae1af6
Conor <conorm2014@gmail.com>
2025-04-24 14:15:25 +0100

    API Code

--- src/app/authconfig.js
+++ src/app/authconfig.js
@@ -2,7 +2,7 @@ export const msalConfig = {
     auth: {
       clientId: "8bbfe0d0-f3ed-44f0-8b80-92be28ad0d51", // Application (client) ID
       authority: "https://login.microsoftonline.com/53e0b897-97cd-48e2-8d7e-4540fd39d742", // Directory (tenant) ID
-      redirectUri: "http://localhost:3000", // or your deployed URL
+      redirectUri: "https://lloydslink2.delightfulsky-983f0008.westus2.azurecontainerapps.io/", // or your deployed URL
     },
     cache: {
       cacheLocation: "sessionStorage", // Can also use "localStorage"
